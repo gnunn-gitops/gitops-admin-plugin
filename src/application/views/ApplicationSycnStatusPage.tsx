8570d1a9bf5ded8f18f319be6aa58290b999d6ec
Gerald Nunn <gerald.b.nunn@gmail.com>
2023-04-20 20:38:10 -0700

    Make better use of spacing

--- src/application/views/ApplicationSycnStatusPage.tsx
+++ src/application/views/ApplicationSycnStatusPage.tsx
@@ -5,7 +5,7 @@ import { K8sGroupVersionKind, ResourceLink, RowProps, TableColumn, TableData, Ti
 import { RouteComponentProps } from 'react-router';
 import { sortable } from '@patternfly/react-table';
 import SyncStatusFragment from './components/Statuses/SyncStatusFragment';
-import { DescriptionList, DescriptionListDescription, DescriptionListGroup, DescriptionListTermHelpText, DescriptionListTermHelpTextButton, PageSection, Popover } from '@patternfly/react-core';
+import { DescriptionList, DescriptionListDescription, DescriptionListGroup, DescriptionListTermHelpText, DescriptionListTermHelpTextButton, Grid, GridItem, PageSection, Popover } from '@patternfly/react-core';
 import { useGitOpsTranslation } from '@gitops-utils/hooks/useGitOpsTranslation';
 import { getDuration } from '@gitops-utils/gitops';
 
@@ -30,93 +30,101 @@ const ApplicationSyncStatusPage: React.FC<ApplicationSyncStatusPageProps> = ({ o
     return (
         <div>
             <PageSection>
-                <DescriptionList>
-                    <DescriptionListGroup>
-                        <DescriptionListTermHelpText>
-                            <Popover headerContent={<div>{t('Operation')}</div>} bodyContent={<div>{t('The operation that was performed.')}</div>}>
-                                <DescriptionListTermHelpTextButton>{t('Operation')}</DescriptionListTermHelpTextButton>
-                            </Popover>
-                        </DescriptionListTermHelpText>
-                        <DescriptionListDescription>
-                            {obj.status?.operationState?.operation?.sync &&
-                                <span>Sync</span>
-                            }
-                        </DescriptionListDescription>
-                    </DescriptionListGroup>
+                <Grid hasGutter={true} span={2} sm={3} md={6} lg={6} xl={6} xl2={6}>
+                    <GridItem>
 
-                    <DescriptionListGroup>
-                        <DescriptionListTermHelpText>
-                            <Popover headerContent={<div>{t('Phase')}</div>} bodyContent={<div>{t('The operation phase.')}</div>}>
-                                <DescriptionListTermHelpTextButton>{t('Phase')}</DescriptionListTermHelpTextButton>
-                            </Popover>
-                        </DescriptionListTermHelpText>
-                        <DescriptionListDescription>
-                            {obj.status?.operationState?.phase}
-                        </DescriptionListDescription>
-                    </DescriptionListGroup>
+                        <DescriptionList>
+                            <DescriptionListGroup>
+                                <DescriptionListTermHelpText>
+                                    <Popover headerContent={<div>{t('Operation')}</div>} bodyContent={<div>{t('The operation that was performed.')}</div>}>
+                                        <DescriptionListTermHelpTextButton>{t('Operation')}</DescriptionListTermHelpTextButton>
+                                    </Popover>
+                                </DescriptionListTermHelpText>
+                                <DescriptionListDescription>
+                                    {obj.status?.operationState?.operation?.sync &&
+                                        <span>Sync</span>
+                                    }
+                                </DescriptionListDescription>
+                            </DescriptionListGroup>
 
-                    <DescriptionListGroup>
-                        <DescriptionListTermHelpText>
-                            <Popover headerContent={<div>{t('Message')}</div>} bodyContent={<div>{t('The message from the operation.')}</div>}>
-                                <DescriptionListTermHelpTextButton>{t('Message')}</DescriptionListTermHelpTextButton>
-                            </Popover>
-                        </DescriptionListTermHelpText>
-                        <DescriptionListDescription>
-                            {obj.status?.operationState?.message || obj.status?.operationState?.message}
-                        </DescriptionListDescription>
-                    </DescriptionListGroup>
+                            <DescriptionListGroup>
+                                <DescriptionListTermHelpText>
+                                    <Popover headerContent={<div>{t('Phase')}</div>} bodyContent={<div>{t('The operation phase.')}</div>}>
+                                        <DescriptionListTermHelpTextButton>{t('Phase')}</DescriptionListTermHelpTextButton>
+                                    </Popover>
+                                </DescriptionListTermHelpText>
+                                <DescriptionListDescription>
+                                    {obj.status?.operationState?.phase}
+                                </DescriptionListDescription>
+                            </DescriptionListGroup>
 
-                    <DescriptionListGroup>
-                        <DescriptionListTermHelpText>
-                            <Popover headerContent={<div>{t('Started At')}</div>} bodyContent={<div>{t('When the operation was started.')}</div>}>
-                                <DescriptionListTermHelpTextButton>{t('Started At')}</DescriptionListTermHelpTextButton>
-                            </Popover>
-                        </DescriptionListTermHelpText>
-                        <DescriptionListDescription>
-                            <Timestamp timestamp={obj.status?.operationState?.startedAt} />
-                        </DescriptionListDescription>
-                    </DescriptionListGroup>
+                            <DescriptionListGroup>
+                                <DescriptionListTermHelpText>
+                                    <Popover headerContent={<div>{t('Message')}</div>} bodyContent={<div>{t('The message from the operation.')}</div>}>
+                                        <DescriptionListTermHelpTextButton>{t('Message')}</DescriptionListTermHelpTextButton>
+                                    </Popover>
+                                </DescriptionListTermHelpText>
+                                <DescriptionListDescription>
+                                    {obj.status?.operationState?.message || obj.status?.operationState?.message}
+                                </DescriptionListDescription>
+                            </DescriptionListGroup>
 
-                    <DescriptionListGroup>
-                        <DescriptionListTermHelpText>
-                            <Popover headerContent={<div>{t('Duration')}</div>} bodyContent={<div>{t('How long the operation took to complete.')}</div>}>
-                                <DescriptionListTermHelpTextButton>{t('Duration')}</DescriptionListTermHelpTextButton>
-                            </Popover>
-                        </DescriptionListTermHelpText>
-                        <DescriptionListDescription>
-                            {obj.status?.operationState?.finishedAt &&
-                                <span>
-                                    {getDuration(obj.status.operationState.startedAt, obj.status.operationState.finishedAt)/1000} seconds
-                                </span>
-                            }
-                        </DescriptionListDescription>
-                    </DescriptionListGroup>
+                            <DescriptionListGroup>
+                                <DescriptionListTermHelpText>
+                                    <Popover headerContent={<div>{t('Initiated By')}</div>} bodyContent={<div>{t('Who initiated the operation.')}</div>}>
+                                        <DescriptionListTermHelpTextButton>{t('Intiated By')}</DescriptionListTermHelpTextButton>
+                                    </Popover>
+                                </DescriptionListTermHelpText>
+                                <DescriptionListDescription>
+                                    {obj.status?.operationState?.operation?.initiatedBy?.automated ? t('automated sync policy') : obj.status?.operationState?.operation?.initiatedBy?.automated}
+                                </DescriptionListDescription>
+                            </DescriptionListGroup>
 
-                    <DescriptionListGroup>
-                        <DescriptionListTermHelpText>
-                            <Popover headerContent={<div>{t('Finished At')}</div>} bodyContent={<div>{t('When the operation was finished.')}</div>}>
-                                <DescriptionListTermHelpTextButton>{t('Finished At')}</DescriptionListTermHelpTextButton>
-                            </Popover>
-                        </DescriptionListTermHelpText>
-                        <DescriptionListDescription>
-                            <Timestamp timestamp={obj.status?.operationState?.finishedAt} />
-                        </DescriptionListDescription>
-                    </DescriptionListGroup>
+                        </DescriptionList>
 
-                    <DescriptionListGroup>
-                        <DescriptionListTermHelpText>
-                            <Popover headerContent={<div>{t('Initiated By')}</div>} bodyContent={<div>{t('Who initiated the operation.')}</div>}>
-                                <DescriptionListTermHelpTextButton>{t('Intiated By')}</DescriptionListTermHelpTextButton>
-                            </Popover>
-                        </DescriptionListTermHelpText>
-                        <DescriptionListDescription>
-                            {obj.status?.operationState?.operation?.initiatedBy?.automated ? t('automated sync policy'):obj.status?.operationState?.operation?.initiatedBy?.automated}
-                        </DescriptionListDescription>
-                    </DescriptionListGroup>
+                    </GridItem>
+                    <GridItem>
+                        <DescriptionList>
+                            <DescriptionListGroup>
+                                <DescriptionListTermHelpText>
+                                    <Popover headerContent={<div>{t('Started At')}</div>} bodyContent={<div>{t('When the operation was started.')}</div>}>
+                                        <DescriptionListTermHelpTextButton>{t('Started At')}</DescriptionListTermHelpTextButton>
+                                    </Popover>
+                                </DescriptionListTermHelpText>
+                                <DescriptionListDescription>
+                                    <Timestamp timestamp={obj.status?.operationState?.startedAt} />
+                                </DescriptionListDescription>
+                            </DescriptionListGroup>
 
-                </DescriptionList>
+                            <DescriptionListGroup>
+                                <DescriptionListTermHelpText>
+                                    <Popover headerContent={<div>{t('Duration')}</div>} bodyContent={<div>{t('How long the operation took to complete.')}</div>}>
+                                        <DescriptionListTermHelpTextButton>{t('Duration')}</DescriptionListTermHelpTextButton>
+                                    </Popover>
+                                </DescriptionListTermHelpText>
+                                <DescriptionListDescription>
+                                    {obj.status?.operationState?.finishedAt &&
+                                        <span>
+                                            {getDuration(obj.status.operationState.startedAt, obj.status.operationState.finishedAt) / 1000} seconds
+                                        </span>
+                                    }
+                                </DescriptionListDescription>
+                            </DescriptionListGroup>
 
+                            <DescriptionListGroup>
+                                <DescriptionListTermHelpText>
+                                    <Popover headerContent={<div>{t('Finished At')}</div>} bodyContent={<div>{t('When the operation was finished.')}</div>}>
+                                        <DescriptionListTermHelpTextButton>{t('Finished At')}</DescriptionListTermHelpTextButton>
+                                    </Popover>
+                                </DescriptionListTermHelpText>
+                                <DescriptionListDescription>
+                                    <Timestamp timestamp={obj.status?.operationState?.finishedAt} />
+                                </DescriptionListDescription>
+                            </DescriptionListGroup>
 
+                        </DescriptionList>
+                    </GridItem>
+                </Grid>
             </PageSection>
             <PageSection>
 
@@ -153,9 +161,6 @@ const applicationListRow: React.FC<RowProps<ApplicationResourceStatus>> = ({ obj
             <TableData id="namespace" activeColumnIDs={activeColumnIDs}>
                 {obj.namespace}
             </TableData>
-            <TableData id="syncWave" activeColumnIDs={activeColumnIDs}>
-                {obj.syncWave}
-            </TableData>
             <TableData id="status" activeColumnIDs={activeColumnIDs}>
                 <SyncStatusFragment
                     status={obj.status}
@@ -186,12 +191,6 @@ export const useResourceColumns = () => {
                 id: 'namespace',
                 transforms: [sortable],
                 sort: `namespace`
-            },
-            {
-                title: 'Sync Order',
-                id: 'syncWave',
-                transforms: [sortable],
-                sort: 'syncWave'
             },
             {
                 title: 'Status',
