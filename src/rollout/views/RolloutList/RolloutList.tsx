9bb809890a3f27d74b7a884bec28ab40f49d7c7d
Gerald Nunn <gerald.b.nunn@gmail.com>
2023-07-27 14:18:36 -0700

    Improve link visualization

--- src/rollout/views/RolloutList/RolloutList.tsx
+++ src/rollout/views/RolloutList/RolloutList.tsx
@@ -97,7 +97,11 @@ const rolloutListRow: React.FC<RowProps<RolloutKind, { namespaceScope: boolean }
         {obj.metadata.labels &&
           <LabelGroup>
             {Object.keys(obj.metadata.labels).map(function (key: string, index) {
-              return <Link to={getSelectorSearchURL(namespaceScope ? obj.metadata.namespace : undefined, "Rollout", key + "=" + obj.metadata.labels[key])}><Label href="javascript:void(0);" color="blue">{key + "=" + obj.metadata.labels[key]}</Label></Link>;
+              return (
+                  <Label color="blue" href="javascript:void(0);">
+                    <Link to={getSelectorSearchURL(namespaceScope ? obj.metadata.namespace : undefined, "Rollout", key + "=" + obj.metadata.labels[key])}>{key + "=" + obj.metadata.labels[key]}</Link>
+                  </Label>
+              );
             })}
           </LabelGroup>
         }
