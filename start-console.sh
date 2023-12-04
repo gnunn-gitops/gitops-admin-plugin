f1a44fe3432d56e6fdb8dac6e05bb0cc885f23e8
Gerald Nunn <gerald.b.nunn@gmail.com>
2023-12-04 13:30:55 -0800

    Update to use 4.14 version instead of latest which is causing some visual issues

--- start-console.sh
+++ start-console.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 npm_package_consolePlugin_name="gitops-admin-plugin"
 
-CONSOLE_IMAGE=${CONSOLE_IMAGE:="quay.io/openshift/origin-console:latest"}
+CONSOLE_IMAGE=${CONSOLE_IMAGE:="quay.io/openshift/origin-console:4.14"}
 CONSOLE_PORT=${CONSOLE_PORT:=9000}
 
 echo "Starting local OpenShift console..."
