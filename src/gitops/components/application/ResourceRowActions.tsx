25be694246347d66405129865c0a1e428f0befef
Gerald Nunn <gerald.b.nunn@gmail.com>
2024-04-01 14:56:59 -0700

    Need to work more on how to call delete dialog with async resource

--- src/gitops/components/application/ResourceRowActions.tsx
+++ src/gitops/components/application/ResourceRowActions.tsx
@@ -1,65 +1,76 @@
 import * as React from 'react';
-import {
-  Dropdown,
-  DropdownItem
-} from '@patternfly/react-core';
 import { ApplicationKind, ApplicationResourceStatus } from '@gitops-models/ApplicationModel';
 import { syncResourcek8s } from '@gitops-services/ArgoCD';
-import { useDeleteModal } from '@openshift-console/dynamic-plugin-sdk';
-import KebabToggle from '@utils/components/toggles/KebabToggle';
+import { useDeleteModal, useK8sModel, k8sGet } from '@openshift-console/dynamic-plugin-sdk';
+import { Dropdown, DropdownItem, KebabToggle } from '@patternfly/react-core/deprecated';
 
 type ResourceRowActionsProps = {
-  resource: ApplicationResourceStatus;
-  application: ApplicationKind;
-  argoBaseURL: string;
-};
-
-function getResourceURL(argoBaseURL: string, resource: ApplicationResourceStatus): string {
-  return argoBaseURL+"?resource=&node=" + encodeURI((resource.group?resource.group:"") + "/" + resource.kind + "/" + (resource.namespace?resource.namespace:"") + "/" + resource.name);
-}
-
-const ResourceRowActions: React.FC<ResourceRowActionsProps> = ({ resource, application, argoBaseURL }) => {
-  const [isOpen, setIsOpen] = React.useState(false);
-
-  const onToggle = () => {
-    setIsOpen((prevIsOpen) => {
-      return !prevIsOpen;
-    });
+    resource: ApplicationResourceStatus;
+    application: ApplicationKind;
+    argoBaseURL: string;
   };
 
-  const launchDeleteModal = useDeleteModal(resource);
+  function getResourceURL(argoBaseURL: string, resource: ApplicationResourceStatus): string {
+    return argoBaseURL+"?resource=&node=" + encodeURI((resource.group?resource.group:"") + "/" + resource.kind + "/" + (resource.namespace?resource.namespace:"") + "/" + resource.name);
+  }
 
-  const onViewResource = () => {
-    window.open(getResourceURL(argoBaseURL, resource), '_blank');
+  const ResourceRowActions: React.FC<ResourceRowActionsProps> = ({ resource, application, argoBaseURL }) => {
+    const [isOpen, setIsOpen] = React.useState(false);
+    const [model] = useK8sModel({ group: resource.group, version: resource.version, kind: resource.kind });
+
+    const getObject = () =>
+        k8sGet({
+            model: model,
+            name: resource.name,
+            ns: resource.namespace,
+        });
+
+    const launchDeleteModal = getObject().then((obj) => {
+        return useDeleteModal(obj);
+    }).catch((error) => {
+        console.log(error);
+    })
+
+
+    const onViewResource = () => {
+      window.open(getResourceURL(argoBaseURL, resource), '_blank');
+    };
+
+    const onSyncResource = () => {
+      syncResourcek8s(application, [resource])
+    };
+
+    const onToggle = (_event: any, isOpen: boolean) => {
+        setIsOpen(isOpen);
+    };
+
+    const onDeleteResource = async () => {
+        launchDeleteModal;
+    };
+
+    return (
+      <Dropdown
+        menuAppendTo={getContentScrollableElement}
+        onSelect={() => setIsOpen(false)}
+        toggle={<KebabToggle onToggle={onToggle} id="toggle-id-disk" />}
+        isOpen={isOpen}
+        isPlain
+        dropdownItems={[
+          <DropdownItem onClick={onViewResource} key="resource-diff">
+            {'Details'}
+          </DropdownItem>,
+          <DropdownItem onClick={onSyncResource} key="resource-sync" isDisabled={resource.status==undefined}>
+            {'Sync'}
+          </DropdownItem>,
+          <DropdownItem onClick={onDeleteResource} key="resource-delete">
+            {'Delete'}
+          </DropdownItem>
+        ]}
+      />
+    );
   };
 
-  const onSyncResource = () => {
-    syncResourcek8s(application, [resource])
-  };
+  export const getContentScrollableElement = (): HTMLElement =>
+    document.getElementById('content-scrollable');
 
-  return (
-    <Dropdown
-      menuAppendTo={getContentScrollableElement}
-      onSelect={() => setIsOpen(false)}
-      toggle={KebabToggle({ isExpanded: isOpen, onClick: onToggle })}
-      isOpen={isOpen}
-      isPlain
-      dropdownItems={[
-        <DropdownItem onClick={onViewResource} key="resource-diff">
-          {'Details'}
-        </DropdownItem>,
-        <DropdownItem onClick={onSyncResource} key="resource-sync" isDisabled={resource.status==undefined}>
-          {'Sync'}
-        </DropdownItem>,
-        <DropdownItem onClick={launchDeleteModal} key="resource-delete">
-          {'Delete'}
-        </DropdownItem>
-      ]}
-    />
-  );
-};
-
-export const getContentScrollableElement = (): HTMLElement =>
-  document.getElementById('content-scrollable');
-
-export default ResourceRowActions;
+  export default ResourceRowActions;
