ce9e0f5ebd9517589db203d3498403dfbab01cc2
Gerald Nunn <gerald.b.nunn@gmail.com>
2024-04-06 15:27:36 -0700

    Cleanup to improve visibility

--- src/gitops/components/project/ProjectAllowDenyTab.tsx
+++ src/gitops/components/project/ProjectAllowDenyTab.tsx
@@ -1,174 +1,146 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import {
-  Card,
-  DescriptionList,
-  DescriptionListDescription,
-  DescriptionListTermHelpText,
-  DescriptionListTermHelpTextButton,
-  List,
-  ListItem,
-  PageSection,
-  PageSectionVariants,
-  Popover,
-  Title
+    Card,
+    CardHeader,
+    CardBody,
+    Grid,
+    GridItem,
+    List,
+    ListItem,
+    Panel,
+    PageSection,
+    PageSectionVariants,
+    Title
 } from '@patternfly/react-core';
 import { useGitOpsTranslation } from '@utils/hooks/useGitOpsTranslation';
 import { AppProjectKind } from '@gitops-models/AppProjectModel';
 import DestinationsList from './DestinationsList';
 import ResourceAllowDenyList from './ResourceAllowDenyList';
+// import DestinationsList from './DestinationsList';
+// import ResourceAllowDenyList from './ResourceAllowDenyList';
 
 function renderStringArray(items: string[]) {
     if (items) {
-      return (
-        <List isPlain className="pf-u-mt-sm">
-          {items.map(el => <ListItem>{el}</ListItem>)}
-        </List>
-      )
+        return (
+            <List isPlain className="pf-u-mt-sm">
+                {items.map(el => <ListItem>{el}</ListItem>)}
+            </List>
+        )
     } else {
-      return (
-        <div className="pf-u-text-align-center pf-u-mt-sm">No Data</div>
-      )
+        return (
+            <div className="pf-u-text-align-center pf-u-mt-sm">Not found</div>
+        )
     }
-  }
+}
 
 type ProjectAllowDenyTabProps = RouteComponentProps<{
     ns: string;
     name: string;
-  }> & {
+}> & {
     obj?: AppProjectKind;
-  };
+};
 
 const ProjectAllowDenyTab: React.FC<ProjectAllowDenyTabProps> = ({ obj }) => {
     const { t } = useGitOpsTranslation();
 
-    const pageSectionStyle = PageSectionVariants.default;
+    const pageSectionStyle = PageSectionVariants.light;
 
     return (
         <>
-        <PageSection variant={pageSectionStyle} hasShadowTop={true}>
-        <Title headingLevel="h2" className="co-section-heading">
-          {t('Source')}
-        </Title>
-            <DescriptionList columnModifier={{ lg: '2Col' }}>
-              {/* <Card isFlat isCompact>
-                <Title headingLevel="h3">
-                    {t('Source')}
+            <PageSection variant={pageSectionStyle} hasShadowTop={true}>
+                <Title headingLevel="h2" className="co-section-heading">
+                    {t('Allowed Sources')}
                 </Title>
-              </Card> */}
-              <Card isFlat isCompact>
-                <DescriptionListTermHelpText>
-                  <Popover headerContent={<div>{t('Repositories')}</div>} bodyContent={<div>{t('Source repositories allowed in this project.')}</div>}>
-                    <DescriptionListTermHelpTextButton>{t('Source repositories allowed in this project.')}</DescriptionListTermHelpTextButton>
-                  </Popover>
-                </DescriptionListTermHelpText>
-                <DescriptionListDescription>
-                  {renderStringArray(obj.spec.sourceRepos)}
-                </DescriptionListDescription>
-              </Card>
-              <Card isFlat isCompact>
-                <DescriptionListTermHelpText>
-                  <Popover headerContent={<div>{t('Namespaces')}</div>} bodyContent={<div>{t('Source namespaces allowed in this project.')}</div>}>
-                    <DescriptionListTermHelpTextButton>{t('Source namespaces allowed in this project.')}</DescriptionListTermHelpTextButton>
-                  </Popover>
-                </DescriptionListTermHelpText>
-                <DescriptionListDescription>
-                  {renderStringArray(obj.spec.sourceNamespaces)}
-                </DescriptionListDescription>
-              </Card>
-            </DescriptionList>
-      </PageSection>
+                <Panel className="pf-v5-u-background-color-200 pf-v5-u-p-md">
+                    <Grid hasGutter span={2} sm={3} md={6} lg={6} xl={6} xl2={6}>
+                        <GridItem>
+                            <Card isFlat isCompact className='pf-v5-u-h-100'>
+                                <CardHeader>
+                                    <Title headingLevel="h5">{t('Repositories')}</Title>
+                                </CardHeader>
+                                <CardBody>{renderStringArray(obj.spec.sourceRepos)}</CardBody>
+                            </Card>
+                        </GridItem>
+                        <GridItem>
+                            <Card isFlat isCompact className='pf-v5-u-h-100'>
+                                <CardHeader>
+                                    <Title headingLevel="h5">{t('Namespaces')}</Title>
+                                </CardHeader>
+                                <CardBody>{renderStringArray(obj.spec.sourceNamespaces)}</CardBody>
+                            </Card>
+                        </GridItem>
+                    </Grid>
+                </Panel>
+            </PageSection>
+            <PageSection variant={pageSectionStyle} hasShadowTop={true}>
+                <Title headingLevel="h2" className="co-section-heading">
+                    {t('Allowed Destinations')}
+                </Title>
 
-      <PageSection variant={pageSectionStyle} hasShadowTop={true}>
-        <Title headingLevel="h2" className="co-section-heading">
-          {t('Destinations')}
-        </Title>
-        <Card isFlat isCompact>
-        <DestinationsList
-          destinations={obj.spec.destinations}
-        />
-        </Card>
-      </PageSection>
+                <Panel className="pf-v5-u-background-color-200 pf-v5-u-p-md">
+                    <Grid hasGutter>
+                        <GridItem>
+                            <Card isFlat isCompact className='pf-v5-u-h-100'>
+                                <CardBody>
+                                    <DestinationsList destinations={obj.spec.destinations} />
+                                </CardBody>
+                            </Card>
+                        </GridItem>
+                    </Grid>
+                </Panel>
+            </PageSection>
 
-      <PageSection hasShadowTop={true} variant={pageSectionStyle} >
-        <Title headingLevel="h2" className="co-section-heading">
-          {t('Resource Allow/Deny Lists')}
-        </Title>
-        <DescriptionList columnModifier={{ lg: '2Col' }}>
-          <Card isFlat isCompact>
-            <DescriptionListTermHelpText>
-              <Popover headerContent={<div>{t('Cluster Resource Allow List')}</div>} bodyContent={<div>{t('Cluster resources allowed in applications in this project.')}</div>}>
-                <DescriptionListTermHelpTextButton>{t('Cluster Resource Allow List')}</DescriptionListTermHelpTextButton>
-              </Popover>
-            </DescriptionListTermHelpText>
-            <DescriptionListDescription>
-              {obj.spec.clusterResourceWhitelist &&
-                <ResourceAllowDenyList
-                  list={obj.spec.clusterResourceWhitelist}
-                />
-              }
-              {!obj.spec.clusterResourceWhitelist &&
-                <div>The cluster resource allow list is empty</div>
-              }
-            </DescriptionListDescription>
-          </Card>
-
-          <Card isFlat isCompact>
-            <DescriptionListTermHelpText>
-              <Popover headerContent={<div>{t('Cluster Resource Deny List')}</div>} bodyContent={<div>{t('Cluster resources denied in applications in this project.')}</div>}>
-                <DescriptionListTermHelpTextButton>{t('Cluster Resource Deny List')}</DescriptionListTermHelpTextButton>
-              </Popover>
-            </DescriptionListTermHelpText>
-            <DescriptionListDescription>
-              {obj.spec.clusterResourceBlacklist &&
-                <ResourceAllowDenyList
-                  list={obj.spec.clusterResourceBlacklist}
-                />
-              }
-              {!obj.spec.clusterResourceBlacklist &&
-                <div>The cluster resource deny list is empty</div>
-              }
-            </DescriptionListDescription>
-          </Card>
-
-          <Card isFlat isCompact>
-            <DescriptionListTermHelpText>
-              <Popover headerContent={<div>{t('Namespace Resource Allow List')}</div>} bodyContent={<div>{t('Namespace resources allowed in applications in this project.')}</div>}>
-                <DescriptionListTermHelpTextButton>{t('Namespace Resource Allow List')}</DescriptionListTermHelpTextButton>
-              </Popover>
-            </DescriptionListTermHelpText>
-            <DescriptionListDescription>
-              {obj.spec.namespaceResourceWhitelist &&
-                <ResourceAllowDenyList
-                  list={obj.spec.namespaceResourceWhitelist}
-                />
-              }
-              {!obj.spec.namespaceResourceWhitelist &&
-                <div>The namespace resource allow list is empty</div>
-              }
-            </DescriptionListDescription>
-          </Card>
-
-          <Card isFlat isCompact>
-            <DescriptionListTermHelpText>
-              <Popover headerContent={<div>{t('Namespace Resource Deny List')}</div>} bodyContent={<div>{t('Namespace resources denied in applications in this project.')}</div>}>
-                <DescriptionListTermHelpTextButton>{t('Namespace Resource Deny List')}</DescriptionListTermHelpTextButton>
-              </Popover>
-            </DescriptionListTermHelpText>
-            <DescriptionListDescription>
-              {obj.spec.namespaceResourceBlacklist &&
-                <ResourceAllowDenyList
-                  list={obj.spec.namespaceResourceBlacklist}
-                />
-              }
-              {!obj.spec.namespaceResourceBlacklist &&
-                <div>The namespace resource deny list is empty</div>
-              }
-            </DescriptionListDescription>
-          </Card>
-        </DescriptionList>
-      </PageSection>
-      </>
+            <PageSection hasShadowTop={true} variant={pageSectionStyle} >
+                <Title headingLevel="h2" className="co-section-heading">
+                    {t('Resource Allow/Deny Lists')}
+                </Title>
+                <Panel className="pf-v5-u-background-color-200 pf-v5-u-p-md">
+                    <Grid hasGutter span={2} sm={3} md={6} lg={6} xl={6} xl2={6}>
+                        <GridItem>
+                            <Card isFlat isCompact className='pf-v5-u-h-100'>
+                                <CardHeader>
+                                    <Title headingLevel="h5">{t('Cluster Resource Allow List')}</Title>
+                                </CardHeader>
+                                <CardBody>
+                                    <ResourceAllowDenyList list={obj.spec.clusterResourceWhitelist} />
+                                </CardBody>
+                            </Card>
+                        </GridItem>
+                        <GridItem>
+                            <Card isFlat isCompact className='pf-v5-u-h-100'>
+                                <CardHeader>
+                                    <Title headingLevel="h5">{t('Cluster Resource Deny List')}</Title>
+                                </CardHeader>
+                                <CardBody>
+                                    <ResourceAllowDenyList list={obj.spec.clusterResourceBlacklist} />
+                                </CardBody>
+                            </Card>
+                        </GridItem>
+                        <GridItem>
+                            <Card isFlat isCompact className='pf-v5-u-h-100'>
+                                <CardHeader>
+                                    <Title headingLevel="h5">{t('Namespace Resource Allow List')}</Title>
+                                </CardHeader>
+                                <CardBody>
+                                    <ResourceAllowDenyList list={obj.spec.namespaceResourceWhitelist} />
+                                </CardBody>
+                            </Card>
+                        </GridItem>
+                        <GridItem>
+                            <Card isFlat isCompact className='pf-v5-u-h-100'>
+                                <CardHeader>
+                                    <Title headingLevel="h5">{t('Namespace Resource Deny List')}</Title>
+                                </CardHeader>
+                                <CardBody>
+                                    <ResourceAllowDenyList list={obj.spec.namespaceResourceBlacklist} />
+                                </CardBody>
+                            </Card>
+                        </GridItem>
+                    </Grid>
+                </Panel>
+            </PageSection>
+        </>
     )
 }
 
