44a75b3aa66f8e57320a5768f27d689659c902cf
Gerald Nunn <gerald.b.nunn@gmail.com>
2024-04-17 18:59:17 -0700

    Refactor how status is handled, more work needed

--- src/externalsecrets/components/ESStatus.tsx
+++ src/externalsecrets/components/ESStatus.tsx
@@ -6,9 +6,8 @@ import {
     YellowExclamationTriangleIcon
 } from '@openshift-console/dynamic-plugin-sdk';
 
-import { SyncUnknownIcon } from '@utils/components/Icons/Icons';
 import { ExternalSecretKind } from '@es-models/ExternalSecrets';
-import { ConditionReason } from '../utils/es-utils';
+import { ExternalSecretStatus, ExternalSecretStatusSeverity, getStatus } from '../utils/es-utils';
 
 interface ESStatusProps {
     externalSecret: ExternalSecretKind
@@ -16,52 +15,20 @@ interface ESStatusProps {
 
 const ESStatus: React.FC<ESStatusProps> = ({ externalSecret:es }) => {
 
-    let targetIcon: React.ReactNode = <SyncUnknownIcon />;
-    let status: string = "Unknown"
-    if (es.status?.conditions) {
-        es.status.conditions.forEach((condition) => {
-            console.log(condition.reason)
-            switch (condition.reason) {
-                case ConditionReason.ConditionReasonSecretSynced:
-                    if (condition.status == "True") {
-                        targetIcon = <GreenCheckCircleIcon/>;
-                        status = "Synced"
-                    } else {
-                        targetIcon = <RedExclamationCircleIcon/>;
-                        status = "Not Synced"
-                    }
-                    break;
-                case (ConditionReason.ConditionReasonSecretSyncedError):
-                    targetIcon = <RedExclamationCircleIcon/>
-                    status = "Sync Error"
-                    break;
-                case (ConditionReason.ConditionReasonSecretDeleted):
-                    targetIcon = <YellowExclamationTriangleIcon/>
-                    status = "Secret Deleted"
-                    break;
-                case (ConditionReason.ReasonInvalidStoreRef):
-                    targetIcon = <RedExclamationCircleIcon/>
-                    status = "Invalid Secret Store"
-                    break;
-                case (ConditionReason.ReasonUpdateFailed):
-                    targetIcon = <RedExclamationCircleIcon/>
-                    status = "Update Failed"
-                    break;
-                case (ConditionReason.ReasonUpdated):
-                    targetIcon = <GreenCheckCircleIcon/>
-                    status = "Synched"
-                case (ConditionReason.ReasonProviderClientConfig):
-                    targetIcon = <RedExclamationCircleIcon/>
-                    status = "Invalid Provider Config"
-                    break;
-            }
-        })
+    let status: ExternalSecretStatus = getStatus(es);
+    let targetIcon: React.ReactNode;
+    if (status.severity == ExternalSecretStatusSeverity.ERROR) {
+        targetIcon = <RedExclamationCircleIcon/>;
+    } else if (status.severity == ExternalSecretStatusSeverity.WARNING) {
+        targetIcon = <YellowExclamationTriangleIcon/>
+    } else {
+        targetIcon = <GreenCheckCircleIcon/>;
     }
 
     return (
         (
             <span>
-                {(status ? targetIcon : "")} {status}
+                {targetIcon} {status.code}
             </span>
         )
     );
--- src/externalsecrets/utils/es-utils.ts
+++ src/externalsecrets/utils/es-utils.ts
@@ -1,3 +1,5 @@
+import { ExternalSecretKind } from "@es-models/ExternalSecrets";
+
 export enum ConditionReason {
     ConditionReasonSecretSynced = "SecretSynced",
 	// ConditionReasonSecretSyncedError indicates that there was an error syncing the secret.
@@ -7,5 +9,68 @@ export enum ConditionReason {
 	ReasonInvalidStoreRef      = "InvalidStoreRef",
 	ReasonProviderClientConfig = "InvalidProviderClientConfig",
 	ReasonUpdateFailed         = "UpdateFailed",
-	ReasonUpdated              = "Updated"
+	ReasonUpdated              = "Updated",
+}
+
+export enum ExternalSecretStatusCode {
+    SYNCED = "Synced",
+    UPDATED = "Updated",
+    NOT_SYNCED = "Not Synced",
+    SYNC_ERROR = "Sync Error",
+    SECRET_DELETED = "Secret Deleted",
+    INVALID_STORE = "Invalid Store",
+    INVALID_PROVIDER_CONFIG = "Invalid Provider Config",
+    UPDATE_FAILED = "Update Failed",
+    UNKNOWN = "Unknown"
+}
+
+export enum ExternalSecretStatusSeverity {
+    HEALTHY = 0,
+    ERROR = 1,
+    WARNING = 2
+}
+
+export type ExternalSecretStatus = {
+    code: ExternalSecretStatusCode
+    severity: ExternalSecretStatusSeverity
+    msg?: string
+}
+
+// TODO - This needs improvement, we iterate but is it necessary?
+export function getStatus(es: ExternalSecretKind): ExternalSecretStatus {
+    let status: ExternalSecretStatus = {code: ExternalSecretStatusCode.UNKNOWN, severity: ExternalSecretStatusSeverity.WARNING}
+    if (es.status?.conditions) {
+        es.status.conditions.forEach((condition) => {
+            switch (condition.reason) {
+                case ConditionReason.ConditionReasonSecretSynced:
+                    if (condition.status == "True") {
+                        status = {code: ExternalSecretStatusCode.SYNCED, severity: ExternalSecretStatusSeverity.HEALTHY}
+                    } else {
+                        status = {code: ExternalSecretStatusCode.NOT_SYNCED, severity: ExternalSecretStatusSeverity.WARNING, msg: condition.message}
+                    }
+                    break;
+                case (ConditionReason.ConditionReasonSecretSyncedError):
+                    status = {code: ExternalSecretStatusCode.SYNC_ERROR, severity: ExternalSecretStatusSeverity.ERROR, msg: condition.message};
+                    break;
+                case (ConditionReason.ConditionReasonSecretDeleted):
+                    status = {code: ExternalSecretStatusCode.SECRET_DELETED, severity: ExternalSecretStatusSeverity.WARNING, msg: condition.message};
+                    break;
+                case (ConditionReason.ReasonInvalidStoreRef):
+                    status = {code: ExternalSecretStatusCode.INVALID_STORE, severity: ExternalSecretStatusSeverity.ERROR, msg: condition.message};
+                    break;
+                case (ConditionReason.ReasonUpdateFailed):
+                    status = {code: ExternalSecretStatusCode.UPDATE_FAILED, severity: ExternalSecretStatusSeverity.ERROR, msg: condition.message};
+                    break;
+                case (ConditionReason.ReasonUpdated):
+                    status = {code: ExternalSecretStatusCode.UPDATED, severity: ExternalSecretStatusSeverity.HEALTHY};
+                    break;
+                case (ConditionReason.ReasonProviderClientConfig):
+                    status = {code: ExternalSecretStatusCode.INVALID_PROVIDER_CONFIG, severity: ExternalSecretStatusSeverity.ERROR, msg: condition.message};
+                    break;
+                default:
+                    status = {code: ExternalSecretStatusCode.UNKNOWN, severity: ExternalSecretStatusSeverity.WARNING, msg: "Unknown condition reason"};
+            }
+        })
+    }
+    return status;
 }
