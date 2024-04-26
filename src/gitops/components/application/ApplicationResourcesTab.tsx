75e88afd0545bb0ed57e346d193b27bd0d427abf
Gerald Nunn <gerald.b.nunn@gmail.com>
2024-04-26 13:09:28 -0700

    Add sync and health filter to resource list, break health into separate column

--- src/gitops/components/application/ApplicationResourcesTab.tsx
+++ src/gitops/components/application/ApplicationResourcesTab.tsx
@@ -7,7 +7,7 @@ import { sortable } from '@patternfly/react-table';
 import SyncStatus from './Statuses/SyncStatus';
 import { PageSection, PageSectionVariants } from '@patternfly/react-core';
 import HealthStatus from './Statuses/HealthStatus';
-import { HealthStatus as HS, SyncStatus as SS } from '@gitops-utils/constants';
+import { HealthStatus as HS } from '@gitops-utils/constants';
 import ResourceRowActions from './ResourceRowActions';
 import { ArgoServer, getArgoServer } from '@gitops-utils/gitops';
 
@@ -26,19 +26,19 @@ const ApplicationResourcesTab: React.FC<ApplicationResourcesTabProps> = ({ obj }
 
     const [model] = useK8sModel({ group: 'route.openshift.io', version: 'v1', kind: 'Route' });
 
-    const [argoServer, setArgoServer] = React.useState<ArgoServer>({host: "", protocol: ""})
+    const [argoServer, setArgoServer] = React.useState<ArgoServer>({ host: "", protocol: "" })
 
     React.useEffect(() => {
-      (async () => {
-        getArgoServer(model, obj)
-          .then((argoServer) => {
-            console.log("Argo Server: " + argoServer);
-            setArgoServer(argoServer);
-          })
-          .catch((err) => {
-            console.error('Error:', err);
-          });
-      })();
+        (async () => {
+            getArgoServer(model, obj)
+                .then((argoServer) => {
+                    console.log("Argo Server: " + argoServer);
+                    setArgoServer(argoServer);
+                })
+                .catch((err) => {
+                    console.error('Error:', err);
+                });
+        })();
     }, [])
 
 
@@ -55,32 +55,32 @@ const ApplicationResourcesTab: React.FC<ApplicationResourcesTabProps> = ({ obj }
         <div>
             <PageSection variant={PageSectionVariants.light}>
                 {obj.metadata &&
-                <ListPageBody>
-                    <ListPageFilter hideNameLabelFilters data={data} loaded={true} rowFilters={filters(resources)} onFilterChange={onFilterChange} />
-                    <VirtualizedTable
-                        data={filteredData}
-                        unfilteredData={resources}
-                        loaded={true}
-                        loadError={null}
-                        columns={useResourceColumns()}
-                        Row={resourceListRow}
-                        rowData={{ application: obj, argoBaseURL: argoServer.protocol + "://" + argoServer.host + "/applications/" + obj?.metadata?.namespace + "/" + obj?.metadata?.name}}
-                    />
-                </ListPageBody>
-            }
+                    <ListPageBody>
+                        <ListPageFilter hideNameLabelFilters data={data} loaded={true} rowFilters={filters(resources)} onFilterChange={onFilterChange} />
+                        <VirtualizedTable
+                            data={filteredData}
+                            unfilteredData={resources}
+                            loaded={true}
+                            loadError={null}
+                            columns={useResourceColumns()}
+                            Row={resourceListRow}
+                            rowData={{ application: obj, argoBaseURL: argoServer.protocol + "://" + argoServer.host + "/applications/" + obj?.metadata?.namespace + "/" + obj?.metadata?.name }}
+                        />
+                    </ListPageBody>
+                }
             </PageSection>
         </div>
     )
 }
 
 const resourceListRow: React.FC<RowProps<
-                                  ApplicationResourceStatus,
-                                  {
-                                    application: ApplicationKind,
-                                    argoBaseURL: string
-                                  }
-                                  >
-                                > = ({ obj, activeColumnIDs, rowData: {application, argoBaseURL} }) => {
+    ApplicationResourceStatus,
+    {
+        application: ApplicationKind,
+        argoBaseURL: string
+    }
+>
+> = ({ obj, activeColumnIDs, rowData: { application, argoBaseURL } }) => {
 
     const gvk: K8sGroupVersionKind = {
         version: obj.version,
@@ -103,7 +103,7 @@ const resourceListRow: React.FC<RowProps<
             <TableData id="syncWave" activeColumnIDs={activeColumnIDs}>
                 {obj.syncWave}
             </TableData>
-            <TableData id="status" activeColumnIDs={activeColumnIDs}>
+            <TableData id="sync-status" activeColumnIDs={activeColumnIDs}>
                 {(obj.health?.status && obj.health.status != HS.HEALTHY) &&
                     <HealthStatus
                         status={obj.health.status}
@@ -113,6 +113,15 @@ const resourceListRow: React.FC<RowProps<
                 <SyncStatus
                     status={obj.status}
                 />
+            </TableData>
+            <TableData id="health-status" activeColumnIDs={activeColumnIDs}>
+                {obj.health?.status &&
+                    <HealthStatus
+                        status={obj.health.status}
+                        message={obj.health.message}
+                    />
+                }
+                {!obj.health?.status && "-"}
             </TableData>
             <TableData
                 id="actions"
@@ -148,15 +157,21 @@ export const useResourceColumns = () => {
                 sort: 'syncWave'
             },
             {
-                title: 'Status',
-                id: 'status',
+                title: 'Sync Status',
+                id: 'sync-status',
                 transforms: [sortable],
                 sort: 'status'
             },
             {
-              title: '',
-              id: 'actions',
-              props: { className: 'dropdown-kebab-pf pf-c-table__action' }
+                title: 'Health Status',
+                id: 'health-status',
+                transforms: [sortable],
+                sort: 'health.status'
+            },
+            {
+                title: '',
+                id: 'actions',
+                props: { className: 'dropdown-kebab-pf pf-c-table__action' }
             }
         ],
         [],
@@ -165,48 +180,76 @@ export const useResourceColumns = () => {
     return columns;
 };
 
-const filters = ( resources: ApplicationResourceStatus[] ): RowFilter[] => {
+const filters = (resources: ApplicationResourceStatus[]): RowFilter[] => {
 
-    return [
-        {
-            filterGroupName: 'Sync Status',
-            type: 'resource-sync',
-            reducer: (resource) => (resource.status),
-            filter: (input, resource) => {
-                if (input.selected?.length && resource?.status) {
-                    return input.selected.includes(resource.status)
-                } else if (!resource?.status) {
-                    return false;
-                }
-                return true;
-            },
-            items: [
-                { id: SS.SYNCED, title: SS.SYNCED },
-                { id: SS.OUT_OF_SYNC, title: SS.OUT_OF_SYNC },
-                { id: SS.UNKNOWN, title: SS.UNKNOWN },
-            ],
-        },
-        {
-            filterGroupName: 'Kind',
-            type: 'resource-kind',
-            reducer: (resource) => (resource.kind),
-            filter: (input, resource) => {
-                if (input.selected?.length) {
-                    return input.selected.includes(resource.kind);
-                } else {
+    return React.useMemo(
+        () => [
+            {
+                filterGroupName: 'Sync Status',
+                type: 'resource-sync',
+                reducer: (resource) => (resource.status),
+                filter: (input, resource) => {
+                    if (input.selected?.length && resource?.status) {
+                        return input.selected.includes(resource.status)
+                    } else if (!resource?.status) {
+                        return false;
+                    }
                     return true;
-                }
+                },
+                items: resources.map((resource) => {
+                    return { id: resource.status, title: resource.status }
+                }).reduce<RowFilterItem[]>(function (result: RowFilterItem[], resource: RowFilterItem) {
+                    if (!result.some(item => item.id === resource.id)) {
+                        result.push(resource);
+                    }
+                    return result;
+                }, [])
             },
-            items: resources.map( (resource) => {
-                return {id: resource.kind, title: resource.kind}
-            }).reduce<RowFilterItem[]>(function (result:RowFilterItem[], resource: RowFilterItem) {
-                if (!result.some(item => item.id === resource.id) ) {
-                    result.push(resource);
-                }
-                return result;
-            }, [])
-        }
-    ];
+            {
+                filterGroupName: 'Health Status',
+                type: 'resource-health',
+                reducer: (resource) => (resource.health ? resource.health.status : "None"),
+                filter: (input, resource) => {
+                    if (input.selected?.length) {
+                        if (resource?.health?.status) {
+                            return input.selected.includes(resource.health.status)
+                        } else if (input.selected.includes("None")) {
+                            return true;
+                        }
+                        return false;
+                    }
+                    return true;
+                },
+                items: resources.map((resource) => {
+                    return { id: resource.health && resource.health.status ? resource.health.status : "None", title: resource.health && resource.health.status ? resource.health.status : "None" }
+                }).reduce<RowFilterItem[]>(function (result: RowFilterItem[], resource: RowFilterItem) {
+                    if (!result.some(item => item.id === resource.id)) {
+                        result.push(resource);
+                    }
+                    return result;
+                }, [])
+            },
+            {
+                filterGroupName: 'Kind',
+                type: 'resource-kind',
+                reducer: (resource) => (resource.kind),
+                filter: (input, resource) => {
+                    if (input.selected?.length) {
+                        return input.selected.includes(resource.kind);
+                    } else {
+                        return true;
+                    }
+                },
+                items: resources.map((resource) => {
+                    return { id: resource.kind, title: resource.kind }
+                }).reduce<RowFilterItem[]>(function (result: RowFilterItem[], resource: RowFilterItem) {
+                    if (!result.some(item => item.id === resource.id)) {
+                        result.push(resource);
+                    }
+                    return result;
+                }, [])
+            }
+        ], [resources]);
 }
 
 export default ApplicationResourcesTab;
